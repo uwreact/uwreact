6834dfb7b75240c87cad8e30b77121eb8d56692c
Michael Midura <michael@midura.io>
2017-10-31 11:14:27 -0400

    Updated update.sh for new setup.

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
-pm2 stop uwri3d.com
+pm2 stop com.uwri3d.client
+pm2 stop com.uwri3d.server
 git pull
 yarn build
-pm2 restart uwri3d.com
+pm2 restart com.uwri3d.client
+pm2 restart com.uwri3d.server
