645b0f8224e31dfe16e518e348ed7ee1cf084e6f
Michael Midura <michael@midura.io>
2017-11-03 12:42:24 -0400

    Ensured token.user === account.email from request.
    This is to ensure that users can only update their application

--- server/data/dataUpdateAccountHandler.js
+++ server/data/dataUpdateAccountHandler.js
@@ -4,11 +4,16 @@ const dataUpdateAccountHandler = async (req, res, next) => {
   const account = req.body.account;
   const accounts = await Account.find({email: req.user.name}, (err, accounts) => accounts);
   if (accounts[0] !== undefined) {
-    accounts[0].information = account.information;
-    accounts[0].buildTeamApplication = account.buildTeamApplication;
-    accounts[0].save();
-    res.set('Content-Type', 'application/json');
-    res.status(200).send(JSON.stringify({type: 'success', message: 'Application Saved!'}));
+    if (account.email === acounts[0].email) {
+      accounts[0].information = account.information;
+      accounts[0].buildTeamApplication = account.buildTeamApplication;
+      accounts[0].save();
+      res.set('Content-Type', 'application/json');
+      res.status(200).send(JSON.stringify({type: 'success', message: 'Application Saved!'}));
+    } else {
+      res.set('Content-Type', 'application/json');
+      res.status(200).send(JSON.stringify({type: 'error', message: 'Token mismatched with request.'}));
+    }
   } else {
     res.set('Content-Type', 'application/json');
     res.status(200).send(JSON.stringify({type: 'error', message: 'Account missing.'}));
