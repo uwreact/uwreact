723a48e671ac1f29463a8cb55b0e027312956a0f
Michael Midura <michael@midura.io>
2017-11-13 23:28:53 -0500

    Reverse PM2 app names

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-pm2 stop com.uwri3d.client
-pm2 stop com.uwri3d.server
+pm2 stop client.uwri3d.com
+pm2 stop server.uwri3d.com
 git pull
 yarn build
-pm2 restart com.uwri3d.client
-pm2 restart com.uwri3d.server
+pm2 restart client.uwri3d.com
+pm2 restart server.uwri3d.com
