30c5bc7bbe3275cf855ed4080b13bdec7854fe6f
Michael Midura <michael@midura.io>
2017-12-28 16:30:04 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
-pm2 stop uwri3d.com/client
-pm2 stop uwri3d.com/server
+pm2 stop uwri3d.com-client
+pm2 stop uwri3d.com-server
 git checkout master
 git pull
 yarn up
 yarn build
-pm2 restart uwri3d.com/client
-pm2 restart uwri3d.com/server
+pm2 restart uwri3d.com-client
+pm2 restart uwri3d.com-server
