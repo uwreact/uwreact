6bbf15cd6bb17959f14814b7db226cbb3fb53688
Michael Midura <michael@midura.io>
2018-09-06 15:05:31 -0400

    Add basic, almost empty view with key phrase and logo

--- merry/src/App/AppView.jsx
+++ merry/src/App/AppView.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const AppView = () => <div>UW REACT</div>;
+import logo from 'resources/svg/full-size-transparent.svg';
+
+import styles from './App.scss';
+
+const AppView = () => (
+  <div className={styles.app}>
+    <div className={styles.fullHeight}>
+      <div className={styles.big}>
+        <span>WE BUILD FULLY AUTONOMOUS FIRST ROBOTS</span>
+      </div>
+      <div className={styles.small}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+      </div>
+    </div>
+  </div>
+);
 
 export default AppView;
