ab60d12ae902b3e8bd519dca59fdf7561c3021f9
Michael Midura <michael@midura.io>
2018-09-06 14:20:49 -0400

    Add connect with router location to AppView to ensure updates on path change

--- merry/src/App/App.jsx
+++ merry/src/App/App.jsx
@@ -1,3 +1,9 @@
-const App = () => '';
+import { connect } from 'react-redux';
+
+import AppView from './AppView';
+
+const mapStateToProps = state => ({ path: state.router.location.pathname });
+
+const App = connect(mapStateToProps)(AppView);
 
 export default App;
