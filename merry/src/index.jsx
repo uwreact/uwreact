2711b75a3be0c8a5f249ec9a9a8099f09ee7edeb
Michael Midura <michael@midura.io>
2018-09-11 12:56:52 -0400

    Import and set up firebase on load

--- merry/src/index.jsx
+++ merry/src/index.jsx
@@ -21,6 +21,27 @@ if (!development) {
   );
 }
 
+import('firebase/app').then(async firebase => {
+  await import('firebase/auth');
+  await import('firebase/firestore');
+  await import('firebase/functions');
+
+  firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  });
+
+  firebase.firestore().settings({ timestampsInSnapshots: true });
+
+  firebase.firestore().enablePersistence();
+
+  firebase.functions();
+});
+
 render(
   <BrowserRouter>
     <App />
