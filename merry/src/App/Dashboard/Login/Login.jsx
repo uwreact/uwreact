8b8ec2d204542a4a78215a52890fe3c51842d448
Michael Midura <michael@midura.io>
2018-09-17 19:58:40 -0400

    Add Login styles

--- merry/src/App/Dashboard/Login/Login.jsx
+++ merry/src/App/Dashboard/Login/Login.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import styles from './Login.scss';
 
-const Login = () => <div className={styles.login}>Login</div>;
+class Login extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signUp: false,
+    };
+  }
+
+  render() {
+    const { signUp } = this.state;
+    console.log(signUp);
+
+    return (
+      <div className={styles.login}>
+        <div className={styles.card}>Login Card</div>
+      </div>
+    );
+  }
+}
 
 export default Login;
