ea9e8c6618119db3ab95374e4f9531dcb2b6b616
Michael Midura <michael@midura.io>
2018-09-17 20:00:35 -0400

    Add TODO for ADFS validation

--- merry/src/App/Dashboard/Login/Login.jsx
+++ merry/src/App/Dashboard/Login/Login.jsx
@@ -2,6 +2,21 @@ import React, { PureComponent } from 'react';
 
 import styles from './Login.scss';
 
+/**
+ * TODO:
+
+ https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize
+ ?client_id=350d760d-cf3a-4aa5-b663-71ad2946ea67
+ &response_type=id_token+token
+ &redirect_uri=https://uwreact.ca/dashboard
+ &scope=openid
+ &response_mode=fragment
+ &state=12345
+ &nonce=678910
+ &prompt=consent
+
+ */
+
 class Login extends PureComponent {
   constructor(props) {
     super(props);
