9721912fb5b56477e55e72616026b73bc5782831
Michael Midura <michael@midura.io>
2018-10-01 13:46:37 -0400

    Replace BlankButton with Link and remove React.Component until necessary

--- merry/src/App/Home/Home.jsx
+++ merry/src/App/Home/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-
-import { BlankButton } from 'components';
+import { Link } from 'react-router-dom';
 
 import horizontalLogo from 'resources/svg/logos/react-horizontal.svg';
 import verticalLogo from 'resources/svg/logos/react-vertical.svg';
@@ -14,38 +12,21 @@ const bigSmall = classNames(styles.fullScreen, styles.bigSmall);
 const bigYellow = classNames(styles.big, styles.yellow);
 const smallWhite = classNames(styles.small, styles.white);
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { history } = this.props;
-
-    return (
-      <div className={styles.home}>
-        <div className={bigSmall}>
-          <div className={classNames(bigYellow, styles.heroText)}>
-            WE BUILD FULLY AUTONOMOUS FIRST ROBOTS.
-          </div>
-          <div className={classNames(smallWhite, styles.heroLogo)}>
-            <img src={horizontalLogo} alt="Logo" className={styles.horizontalLogo} />
-            <img src={verticalLogo} alt="Logo" className={styles.verticalLogo} />
-          </div>
-        </div>
-        <BlankButton className={styles.interested} onClick={() => history.push('/apply')}>
-          INTERESTED? APPLY NOW!
-        </BlankButton>
+const Home = () => (
+  <div className={styles.home}>
+    <div className={bigSmall}>
+      <div className={classNames(bigYellow, styles.heroText)}>
+        WE BUILD FULLY AUTONOMOUS FIRST ROBOTS.
       </div>
-    );
-  }
-}
-
-Home.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
-};
+      <div className={classNames(smallWhite, styles.heroLogo)}>
+        <img src={horizontalLogo} alt="Logo" className={styles.horizontalLogo} />
+        <img src={verticalLogo} alt="Logo" className={styles.verticalLogo} />
+      </div>
+    </div>
+    <Link className={styles.interested} to="/apply">
+      INTERESTED? APPLY NOW!
+    </Link>
+  </div>
+);
 
 export default Home;
