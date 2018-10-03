c3f2aada985d24e3a5408ce0c1ebeda9a56ef113
Michael Midura <michael@midura.io>
2018-10-03 11:39:17 -0400

    Simplify styles to allow content to overflow

--- merry/src/App/Home/Home.jsx
+++ merry/src/App/Home/Home.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
 
 import horizontalLogo from 'resources/svg/logos/react-horizontal.svg';
 import verticalLogo from 'resources/svg/logos/react-vertical.svg';
 
 import styles from './Home.scss';
 
-const bigSmall = classNames(styles.fullScreen, styles.bigSmall);
-
-const bigYellow = classNames(styles.big, styles.yellow);
-const smallWhite = classNames(styles.small, styles.white);
-
 const Home = () => (
   <div className={styles.home}>
-    <div className={bigSmall}>
-      <div className={classNames(bigYellow, styles.heroText)}>
+    <div className={styles.content}>
+      <div className={classNames(styles.primary, styles.heroText)}>
         WE BUILD FULLY AUTONOMOUS FIRST ROBOTS.
       </div>
-      <div className={classNames(smallWhite, styles.heroLogo)}>
-        <img src={horizontalLogo} alt="Logo" className={styles.horizontalLogo} />
-        <img src={verticalLogo} alt="Logo" className={styles.verticalLogo} />
+      <div className={styles.white}>
+        <div className={styles.contentLogo}>
+          <img src={horizontalLogo} alt="UW REACT Logo" />
+        </div>
       </div>
     </div>
-    <Link className={styles.interested} to="/apply">
-      INTERESTED? APPLY NOW!
-    </Link>
+    <div className={styles.menu}>
+      <div className={styles.menuLogo}>
+        <img src={verticalLogo} alt="UW REACT Logo" />
+      </div>
+    </div>
   </div>
 );
 
