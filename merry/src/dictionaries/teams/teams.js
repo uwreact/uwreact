1d7e1ca9cedaa4e4f5392641f263eaf534590a7c
Michael Midura <michael@midura.io>
2018-10-09 09:20:57 -0400

    Add infrastructure team

--- merry/src/dictionaries/teams/teams.js
+++ merry/src/dictionaries/teams/teams.js
@@ -2,6 +2,7 @@ const teams = [
   'Software - Perception',
   'Software - Planning',
   'Software - Controls',
+  'Software - Infrastructure',
   'Mechanical - Design',
   'Mechanical - Manufacturing',
   'Business - Logistics',
