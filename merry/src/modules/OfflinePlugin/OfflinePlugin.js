45dc81e3ec6ac71b2ca4027627dc8d9863970a77
Michael Midura <michael@midura.io>
2018-11-05 13:49:02 -0500

    Take control of ServiceWorker using events

--- merry/src/modules/OfflinePlugin/OfflinePlugin.js
+++ merry/src/modules/OfflinePlugin/OfflinePlugin.js
@@ -5,7 +5,22 @@ const OfflinePlugin = new Importable(
   async modules => {
     const [offlinePluginRuntime] = modules;
 
-    offlinePluginRuntime.install();
+    offlinePluginRuntime.install({
+      onUpdating: () => {
+        console.log('SW Event:', 'onUpdating');
+      },
+      onUpdateReady: () => {
+        console.log('SW Event:', 'onUpdateReady');
+        offlinePluginRuntime.applyUpdate();
+      },
+      onUpdated: () => {
+        console.log('SW Event:', 'onUpdated');
+        window.location.reload();
+      },
+      onUpdateFailed: () => {
+        console.log('SW Event:', 'onUpdateFailed');
+      },
+    });
   },
   async modules => {
     const [offlinePluginRuntime] = modules;
