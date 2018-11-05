d06ce558d603ffa2a06e828d2be2cc36fe161acb
Michael Midura <michael@midura.io>
2018-11-05 11:54:12 -0500

    Move decryption to before deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,4 @@ echo HELLO
 echo "HELLO"
 echo "$TRAVIS_BRANCH"
 
-openssl aes-256-cbc -K $encrypted_5880cf525281_key -iv $encrypted_5880cf525281_iv -in secrets.zip.enc -out secrets.zip -d
-
 yarn deploy
